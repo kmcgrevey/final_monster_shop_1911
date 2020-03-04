a97c1d41ca7e807d0b648c7c07c9ed5661597412
Kevin McGrevey <niv3kmcg@gmail.com>
2020-03-03 21:32:21 -0700

    Enable to create a discount

--- app/controllers/merchant_employee/discounts_controller.rb
+++ app/controllers/merchant_employee/discounts_controller.rb
@@ -1,25 +1,21 @@
 class MerchantEmployee::DiscountsController < ApplicationController
 # class MerchantEmployee::DiscountsController < MerchantEmployee::BaseController
 
+# similar coding as ItemsController
   def new
     @merchant = Merchant.find(params[:id])
   end
 
   def create
-    # discount = Discount.create(discount_params)
-  #   if merchant.save
-  #     redirect_to merchants_path
-  #   else
-  #     flash[:error] = merchant.errors.full_messages.to_sentence
-  #     render :new
-  #   end
+    @merchant = Merchant.find(params[:id])
+    @discount = @merchant.discounts.create(discount_params)
   end
 
-  # private
-  #
-  # def discount_params
-  #   params.permit(:name,
-	# 		            :percent_off,
-	# 		            :bulk_minimum)
-  # end
+  private
+
+  def discount_params
+    params.permit(:name,
+			            :percent_off,
+			            :bulk_minimum)
+  end
 end
